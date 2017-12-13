7bca82d8a3bbb5c8e7d14316e88e724952fb5908
s848 <gsan@umich.edu>
2017-12-13 17:49:50 -0500

    example

--- tfocs_ex.py
+++ tfocs_ex.py
@@ -11,23 +11,29 @@ import numpy as np
 import numpy.linalg as LA
 
 data = np.genfromtxt('diabetes.csv', delimiter=',')
-X = np.array(data[:, :-1], dtype='float')
-Y = np.array(data[:, -1], dtype='int')
-X=np.hstack((np.ones((np.shape(X)[0],1)),X))
-Xd=np.delete(X,0,axis=1)
-Xd=np.hstack((Xd,np.ones((np.shape(X)[0],1))))
-s = 0
+#X = np.array(data[:, :-1], dtype='float')
+#Y = np.array(data[:, -1], dtype='int')
+n, p = 4000, 100
+k = 5
+X = np.random.normal(size = (n, p))
+beta = np.append(np.zeros(p - k), np.full(k, 10))
+error = np.random.normal(scale = 0.01, size = n)
+#X=np.hstack((np.ones((np.shape(X)[0],1)),X))
+Y = X.dot(beta) + error
+
+s=0
+#s = LA.norm(np.dot(X.T,error), ord = np.inf)
 
 def smoothF(b):
     return (LA.norm(Y-np.dot(X,b)))/2.0
 def gradF(b):
     return np.dot(np.transpose(X),np.dot(X,b))-np.dot(np.transpose(X),Y)
 def nonsmoothF(b):
-    return l*LA.norm(b,1)
+    return s*LA.norm(b,1)
 def projectorF(b,t):
     return np.where(b<-s*t,b+s*t,np.where(abs(b)<=s*t,0,b-s*t))
 
-a,b = tfocs(smoothF, gradF, nonsmoothF, projectorF,np.array([1,0,0,0,0,0,0,0,0,0,0]), 
+a,b = tfocs(smoothF, gradF, nonsmoothF, projectorF,np.zeros(p), 
           tol = 1e-8, gamma = 1e-4, method = 'AT')
 
 
