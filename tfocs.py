0ec1055c8c8fb16fd289e4a0f6016af00fbff752
Caleb <calebki94@gmail.com>
2017-12-16 14:51:35 -0500

    added updated function file

--- tfocs.py
+++ tfocs.py
@@ -7,9 +7,10 @@ Created on Mon Nov 20 20:52:15 2017
 import numpy as np
 import numpy.linalg as LA
 
-def tfocs(smoothF, gradF, nonsmoothF, projectorF, x0, 
-          tol = 1e-8, gamma = 1e-4, method = 'AT'):
-    """Returns optimal point and solution to optimization problem
+def tfocsAT(smoothF, gradF, nonsmoothF, projectorF, x0, 
+          tol = 1e-8, gamma = 1e-2):
+    """Returns optimal point and solution to optimization problem using 
+       Auslender and Teboulle's method
     
     Keyword arguments:
         smoothF -- a smooth function
@@ -20,8 +21,6 @@ def tfocs(smoothF, gradF, nonsmoothF, projectorF, x0,
         tol -- tolerance threshold for convergence of optimization problem 
                 (default 1e-8)
         gamma -- threshold for deciding how to backtrack using L (default 1e-5)
-        method -- 'AT' for Auslender and Teboulle's method and 'LLM' for Lan, 
-                  Lu, and Monteiro's method
     """
     alpha = 0.9
     beta = 0.5
@@ -29,26 +28,22 @@ def tfocs(smoothF, gradF, nonsmoothF, projectorF, x0,
     LNew = 1
     xNew = x0
     xBarNew = x0
-    
-    counter = 0
+    itercount = 0
     
     while True:
+        itercount = itercount + 1
         LOld = LNew
         LNew = LOld * alpha
         xOld = xNew
         xBarOld = xBarNew
         thetaOld = thetaNew
-
        
         while True:
             thetaNew = 2/(1 + np.sqrt(1 + 4*LNew / (np.power(thetaOld,2) * LOld)))
             y = (1 - thetaNew) * xOld + thetaNew * xBarOld
             xBarNew = projectorF(xBarNew - gradF(y)/(LNew * thetaNew), 
                                   1/(LNew * thetaNew))
-            if method == 'AT':
-                xNew = (1 - thetaNew) * xOld +  thetaNew * xBarNew
-            elif method == 'LLM':
-                xNew = projectorF(y - gradF(y)/LNew, 1/LNew)
+            xNew = (1 - thetaNew) * xOld +  thetaNew * xBarNew
                 
             if smoothF(y) - smoothF(xNew) >= gamma * smoothF(xNew):                        
                 LHat = \
@@ -62,18 +57,67 @@ def tfocs(smoothF, gradF, nonsmoothF, projectorF, x0,
                 break
             LNew = max(LNew/beta, LHat)
             
-            
-        '''    
-        print("Step size: ", LOld)
-        print("Function value: ", smoothF(xNew) + nonsmoothF(xNew))
-        print("Difference between iterates: ", LA.norm(xNew - xOld))    
-        '''
+
         if LA.norm(xNew - xOld, 2)/max(1, LA.norm(xNew)) <= tol:
             break
-        
-        #if counter > 10:
-            #break
     
-    return np.array([xNew, smoothF(xNew) + nonsmoothF(xNew)])
+    xNew = projectorF(xNew - gradF(xNew)/LNew, 1/LNew)
+    return np.array([xNew, smoothF(xNew) + nonsmoothF(xNew), itercount])
     
+def tfocsLLM(smoothF, gradF, nonsmoothF, projectorF, x0, 
+          tol = 1e-8, gamma = 1e-2):    
+    """Returns optimal point and solution to optimization problem using 
+       Lan, Lu, and Monteiro's method
     
+    Keyword arguments:
+        smoothF -- a smooth function
+        gradF -- the gradient of the smooth function
+        nonsmoothF -- a nonsmooth function
+        projectorF -- prox function of the nonsmooth function
+        x0 -- initial starting point
+        tol -- tolerance threshold for convergence of optimization problem 
+                (default 1e-8)
+        gamma -- threshold for deciding how to backtrack using L (default 1e-5)
+    """
+    alpha = 0.9
+    beta = 0.5
+    thetaNew = 1
+    LNew = 1
+    xNew = x0
+    xBarNew = x0
+    itercount = 0
+    
+    while True:
+        itercount = itercount + 1
+        LOld = LNew
+        LNew = LOld * alpha
+        xOld = xNew
+        xBarOld = xBarNew
+        thetaOld = thetaNew
+
+       
+        while True:
+            thetaNew = 2/(1 + np.sqrt(1 + 4*LNew / (np.power(thetaOld,2) * LOld)))
+            y = (1 - thetaNew) * xOld + thetaNew * xBarOld
+            xBarNew = projectorF(xBarNew - gradF(y)/(LNew * thetaNew), 
+                                  1/(LNew * thetaNew))
+            
+            xNew = projectorF(y - gradF(y)/LNew, 1/LNew)
+                
+            if smoothF(y) - smoothF(xNew) >= gamma * smoothF(xNew):                        
+                LHat = \
+                2*(smoothF(xNew) - smoothF(y) - (gradF(y).dot(xNew - y))) \
+                /np.power(LA.norm(xNew - y),2) 
+            else:
+                LHat = 2 * np.absolute(
+                        np.dot(y - xNew, gradF(xNew) - gradF(y))) \
+                        /np.power(LA.norm(xNew - y),2)
+            if LNew >= LHat:
+                break
+            LNew = max(LNew/beta, LHat)
+            
+
+        if LA.norm(xNew - xOld, 2)/max(1, LA.norm(xNew)) <= tol:
+            break
+
+    return np.array([xNew, smoothF(xNew) + nonsmoothF(xNew), itercount])
