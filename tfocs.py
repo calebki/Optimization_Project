4c4e987d5181befdf4a85c6f1aa168ace67c4e1d
Caleb <calebki94@gmail.com>
2017-12-17 19:52:13 -0500

    added updates and sim files

--- tfocs.py
+++ tfocs.py
@@ -7,8 +7,8 @@ Created on Mon Nov 20 20:52:15 2017
 import numpy as np
 import numpy.linalg as LA
 
-def tfocsAT(smoothF, gradF, nonsmoothF, projectorF, x0, 
-          tol = 1e-8, gamma = 1e-2):
+def tfocs(smoothF, gradF, nonsmoothF, projectorF, x0, 
+          tol = 1e-8, gamma = 1e-2, solver = 'AT'):
     """Returns optimal point and solution to optimization problem using 
        Auslender and Teboulle's method
     
@@ -43,8 +43,12 @@ def tfocsAT(smoothF, gradF, nonsmoothF, projectorF, x0,
             y = (1 - thetaNew) * xOld + thetaNew * xBarOld
             xBarNew = projectorF(xBarNew - gradF(y)/(LNew * thetaNew), 
                                   1/(LNew * thetaNew))
-            xNew = (1 - thetaNew) * xOld +  thetaNew * xBarNew
-                
+            
+            if solver == 'AT':
+                xNew = (1 - thetaNew) * xOld +  thetaNew * xBarNew
+            else:
+                xNew = projectorF(y - gradF(y)/LNew, 1/LNew)
+            
             if smoothF(y) - smoothF(xNew) >= gamma * smoothF(xNew):                        
                 LHat = \
                 2*(smoothF(xNew) - smoothF(y) - (gradF(y).dot(xNew - y))) \
@@ -61,63 +65,7 @@ def tfocsAT(smoothF, gradF, nonsmoothF, projectorF, x0,
         if LA.norm(xNew - xOld, 2)/max(1, LA.norm(xNew)) <= tol:
             break
     
-    xNew = projectorF(xNew - gradF(xNew)/LNew, 1/LNew)
-    return np.array([xNew, smoothF(xNew) + nonsmoothF(xNew), itercount])
-    
-def tfocsLLM(smoothF, gradF, nonsmoothF, projectorF, x0, 
-          tol = 1e-8, gamma = 1e-2):    
-    """Returns optimal point and solution to optimization problem using 
-       Lan, Lu, and Monteiro's method
-    
-    Keyword arguments:
-        smoothF -- a smooth function
-        gradF -- the gradient of the smooth function
-        nonsmoothF -- a nonsmooth function
-        projectorF -- prox function of the nonsmooth function
-        x0 -- initial starting point
-        tol -- tolerance threshold for convergence of optimization problem 
-                (default 1e-8)
-        gamma -- threshold for deciding how to backtrack using L (default 1e-5)
-    """
-    alpha = 0.9
-    beta = 0.5
-    thetaNew = 1
-    LNew = 1
-    xNew = x0
-    xBarNew = x0
-    itercount = 0
-    
-    while True:
-        itercount = itercount + 1
-        LOld = LNew
-        LNew = LOld * alpha
-        xOld = xNew
-        xBarOld = xBarNew
-        thetaOld = thetaNew
-
-       
-        while True:
-            thetaNew = 2/(1 + np.sqrt(1 + 4*LNew / (np.power(thetaOld,2) * LOld)))
-            y = (1 - thetaNew) * xOld + thetaNew * xBarOld
-            xBarNew = projectorF(xBarNew - gradF(y)/(LNew * thetaNew), 
-                                  1/(LNew * thetaNew))
-            
-            xNew = projectorF(y - gradF(y)/LNew, 1/LNew)
-                
-            if smoothF(y) - smoothF(xNew) >= gamma * smoothF(xNew):                        
-                LHat = \
-                2*(smoothF(xNew) - smoothF(y) - (gradF(y).dot(xNew - y))) \
-                /np.power(LA.norm(xNew - y),2) 
-            else:
-                LHat = 2 * np.absolute(
-                        np.dot(y - xNew, gradF(xNew) - gradF(y))) \
-                        /np.power(LA.norm(xNew - y),2)
-            if LNew >= LHat:
-                break
-            LNew = max(LNew/beta, LHat)
-            
-
-        if LA.norm(xNew - xOld, 2)/max(1, LA.norm(xNew)) <= tol:
-            break
-
+    if solver == 'AT':
+        xNew = projectorF(xNew - gradF(xNew)/LNew, 1/LNew)
+   
     return np.array([xNew, smoothF(xNew) + nonsmoothF(xNew), itercount])
--- tfocs_ex.py
+++ tfocs_ex.py
@@ -5,48 +5,56 @@ Created on Mon Dec 11 20:55:09 2017
 
 @author: Sanjana
 """
-
+from tfocs import tfocs
 import numpy as np
 import numpy.linalg as LA
+import time
+import pandas as pd
 
+df = pd.DataFrame(columns = ['n', 'p', 'k', 'sigma', 'lambda', 
+                        'method', 'time', 'error', 'numiters'])
+settings = np.array([[500, 100, 10, 1], [50, 100, 10, 1], 
+                     [500, 100, 10, 0.1], [50, 100, 10, 0.1]], dtype = object)
+numsim = 1
 
-data = np.genfromtxt('diabetes.csv', delimiter=',')
-X = np.array(data[:, :-1], dtype='float')
-Y = np.array(data[:, -1], dtype='int')
-X=np.hstack((np.ones((np.shape(X)[0],1)),X))
-p=11
+counter = 0
 
+for n, p, k, sigma in settings:
+    
+    X = np.random.normal(size = (n, p))
+    beta = np.append(np.zeros(p - k), np.full(k, 10))
+    error = np.random.normal(scale = sigma, size = n)
+    Y = X.dot(beta) + error
 
-'''
-n, p = 400, 100
-k = 10
-X = np.random.normal(size = (n, p))
-beta = np.append(np.zeros(p - k), np.full(k, 10))
-error = np.random.normal(scale = 0.1, size = n)
-Y = X.dot(beta) + error
-'''
+    lambdas = [0, LA.norm(np.dot(X.T,error), ord = np.inf)/2, 
+         LA.norm(np.dot(X.T,error), ord = np.inf) + 0.001]
 
-s=60
-#s = LA.norm(np.dot(X.T,error), ord = np.inf) + 0.0001
+    for s in lambdas:
+        def smoothF(b):
+            return (LA.norm(Y-np.dot(X,b)))/2.0
+        def gradF(b):
+            return np.dot(np.transpose(X),np.dot(X,b))-np.dot(np.transpose(X),Y)
+        def nonsmoothF(b):
+            return s*LA.norm(b,1)
+        def projectorF(b,t):
+            return np.where(b < -s * t, b + s * t, 
+                            np.where(abs(b) <= s * t, 0, b - s * t))
+            
+        for i in range(0,numsim):
+            
+            t0 = time.time()
+            bstarAT, pstarAT, countAT = tfocs(smoothF, gradF, nonsmoothF, projectorF, 
+                                              np.zeros(p, dtype = np.longdouble), 
+                                              tol = 1e-8, gamma = 1, solver = 'AT')
+            t1 = time.time()
+            bstarLLM, pstarLLM, countLLM = tfocs(smoothF, gradF, nonsmoothF, projectorF, 
+                                                 np.zeros(p, dtype = np.longdouble), 
+                                                 tol = 1e-8, gamma = 1, solver = 'LLM')
+            t2= time.time()
+            df.loc[counter] = [n, p, k, sigma, s, 'AT', t1 - t0, 
+                  LA.norm(beta - bstarAT), countAT]
+            df.loc[counter + 1] = [n, p, k, sigma, s, 'LLM', t2 - t1, 
+                  LA.norm(beta - bstarLLM), countLLM]
+            counter = counter + 2
 
-def smoothF(b):
-    return (LA.norm(Y-np.dot(X,b)))/2.0
-def gradF(b):
-    return np.dot(np.transpose(X),np.dot(X,b))-np.dot(np.transpose(X),Y)
-def nonsmoothF(b):
-    return s*LA.norm(b,1)
-def projectorF(b,t):
-    return np.where(b<-s*t,b+s*t,np.where(abs(b)<=s*t,0,b-s*t))
-
-a,b = tfocs(smoothF, gradF, nonsmoothF, projectorF,np.zeros(p), 
-          tol = 1e-10, gamma = 1e-2, method = 'AT')
-
-
-'''
-def SoftThreshold(x,y):
-    return np.where(x<-y,x+y,np.where(abs(x)<=y,0,x-y))
-def prox(x,t):
-    return SoftThreshold(x,s*t)
-def projectorF(b,s):
-    return prox(b,s)
-''' 
+df.to_csv("simdata.csv")
